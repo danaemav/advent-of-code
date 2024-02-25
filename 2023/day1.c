44325822e410cd3fbcc4425f34a66e8344e48cb7
Danai Mavromati <sdi2300119@di.uoa.gr>
2024-02-25 21:06:41 +0200

    commit 2

--- 2023/day1.c
+++ 2023/day1.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-void find_calibration(int content, int result){
+// function that finds the first and last digits from a line and calculates the calibration value //
+int find_calibration(int *line, int size){
     
+    int dig1 = 0, dig2 = 0;
+    int pos1 = 0;
+    
+    for(int i = 0; i < size){
+        if(isdigit(line[i])){   // search for a digit starting from the front of the line //
+            dig1 = content[i];
+            pos1 = i;           // if it is found, store its position and break loop //
+            break;
+        }
+    }
+    if (posi != 0) {                                // if a digit exists //
+        for(int i = size ; i > posi; i--){          // iterate from the back of line to the position of digit1 //
+            if(isdigit(line[i])){
+                dig2 = content[i];
+                break;
+            }
+        }
+    } else {
+        return 0;   // if there is no digit found, the calibration value of the line is 0 //
+    }
+
+    dig2 = (dig2 == 0) ? dig1 : dig2;   // in the case that only 1 digit exists, the digit2 equals to digit1 //
+
+    int result = dig1 * 10 + dig2; 
+    return result;
 }
 
 
-
 int main(void){
+    
     FILE *fp = fopen(stdin, "r");
     if (fp1 == NULL) {   // check if file opened correctly //
         fprintf(stderr, "Error: cannot open file\n");
@@ -34,5 +61,5 @@ int main(void){
     content[size] = 0;
 
 
-
+    return 0;
 }
