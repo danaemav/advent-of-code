f1c3260968feeaf212f84b166955eb7701711a65
Danai Mavromati <sdi2300119@di.uoa.gr>
2024-02-25 21:26:02 +0200

    update

--- 2023/day1.c
+++ 2023/day1.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 // function that finds the first and last digits from a line and calculates the calibration value //
-int find_calibration(int *line){
+int find_calibration(int line[]){
     
     int size = strlen(line);
     int dig1 = 0, dig2 = 0;
     int pos1 = 0;
     
     for(int i = 0; i < size){
-        if(isdigit(line[i])){   // search for a digit starting from the front of the line //
-            dig1 = content[i];
+        if(isdigit(line[i])) {   // search for a digit starting from the front of the line //
+            dig1 = line[i];
             pos1 = i;           // if it is found, store its position and break loop //
             break;
         }
     }
-    if (posi != 0) {                                // if a digit exists //
-        for(int i = size ; i > posi; i--){          // iterate from the back of line to the position of digit1 //
+    if (pos1 != 0) {                                // if a digit exists //
+        for(int i = size ; i > pos1; i--){          // iterate from the back of line to the position of digit1 //
             if(isdigit(line[i])){
-                dig2 = content[i];
+                dig2 = line[i];
                 break;
             }
         }
@@ -36,17 +37,19 @@ int find_calibration(int *line){
 
 int main(void){
     
-    FILE *fp = fopen(stdin, "r");
-    if (fp1 == NULL) {   // check if file opened correctly //
+    // attempt to open the file and check if it opened succesfully //
+    FILE *fp = freopen(NULL, "r", stdin);
+    if (fp == NULL) {
         fprintf(stderr, "Error: cannot open file\n");
         exit(1);
     }
 
     // read each line of the file and call find_calibration in order to get each value and sum later //
+    // things missing - must do this for all lines //
 
     int line[1024];
     int sum = 0;
-    while (fscanf(file, "%1023d", line) != EOF){
+    while (fscanf(fp, "%1023d", line) != EOF){
         int result = find_calibration(line);
         fprintf(stderr,"%d", result);
         sum+=result ;
