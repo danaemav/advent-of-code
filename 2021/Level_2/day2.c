5982096d1c10a2bfdf23a032cd7823cdf2295a51
Danai Mavromati <sdi2300119@di.uoa.gr>
2024-03-02 01:15:48 +0200

    day 2 pt.a finished

--- 2021/Level_2/day2.c
+++ 2021/Level_2/day2.c
@@ -1,56 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 #define MAX_LINE_CHARS 100
 
-
-void calculations(char line[], int horizontal, int depth){
-    
-    int size = strlen(line);
-    
-    for (int i = 0; i < size; i++){
-        switch(line[i]){
-            case 'f':
-                horizontal = horizontal + (line[i + 8] - '0');
-                break;
-            case 'd':
-                depth = depth + (line[i + 5] - '0');
-                break;
-            case 'u':
-                depth = depth - (line[i + 3] - '0');
-                break;
-        }
+void calculations(char line[], int *horizontal, int *depth) {
+    // according to the first letter, update the horizontal and depth position //
+    switch (line[0]) {
+        case 'f':
+            *horizontal += (line[8] - '0');
+            break;
+        case 'd':
+            *depth += (line[5] - '0');
+            break;
+        case 'u':
+            *depth -= (line[3] - '0');
+            break;
     }
-    printf("horizontal : %d , depth : %d\n", horizontal, depth);
-
+    printf("Current position: %d , %d\n", *horizontal, *depth);
 }
 
 
-
-int main(void){
+int main(void) {
     
-    // attempt to open the file and check if it opened succesfully //
+    // check if the file opened successfully //
     FILE *fp = freopen(NULL, "r", stdin);
     if (fp == NULL) {
         fprintf(stderr, "Error: cannot open file\n");
         exit(1);
     }
-    
+
     char line[MAX_LINE_CHARS];
-    int horizontal = 0, depth = 0 ;
+    int horizontal = 0, depth = 0;
     
     // read each line of the file //
     while (fgets(line, sizeof(line), fp) != NULL) {
-        
-        calculations(line, horizontal, depth);
-
+        calculations(line, &horizontal, &depth);
     }
 
-    int result = horizontal * depth; 
-
-    fprintf(stderr, "Result final = %d\n", result );
+    int result = horizontal * depth ;
+    fprintf(stderr, "Result = %d\n", result);
 
     fclose(fp);
     return 0;
